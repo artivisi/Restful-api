fc659a5718a6ea84f9850479e8a04f8c4e00255c
jimmy rengga <rengga.jimmy@gmail.com>
2016-03-31 11:35:42 +0700

    init restful-demo menggunakan basic-auth

---
+++ basic-auth-demo/src/main/java/com/artivisi/auth/basic/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.artivisi.auth.basic.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String allCategory() {
+        return "Halo Kawan Kawan";
+    }
+    
+
+}
